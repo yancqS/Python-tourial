089c18810ccacd66ff196e25b95419a1ef25e521
闫崇庆 <23746485@qq.com>
2021-09-02 21:53:04 +0800

    feat: 更新CH1代码部分 :tada:

---
+++ CH1/Variables-and-simple-data-types.py
@@ -0,0 +1,28 @@
+first_name = 'John'
+last_name = 'Tom'
+full_name = f"{ first_name } { last_name }"
+print(full_name)
+
+name = " python "
+print(name)
+print(name.strip(), end="-\n")
+print(name.rstrip(), end="-\n")
+print(name.lstrip(), end="-\n")
+
+my_name = "albert Einstein"
+print(my_name.lower())
+print(my_name.upper())
+print(my_name.title())
+
+print(f'hello { my_name }, would you like to learn some Python today')
+
+famous_name = "Albert Einstein"
+
+print(f'{ famous_name } once said, "A person who nerver made a mistake never tried anything new"')
+
+his_name = "\tdacongming\n"
+
+print(his_name)
+print(his_name.strip())
+print(his_name.lstrip())
+print(his_name.rstrip())
