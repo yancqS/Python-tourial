ae5394c5abb84e99fcb7c49fe9f6b80b15bfc361
闫崇庆 <23746485@qq.com>
2021-10-21 21:31:05 +0800

    chore: add res.html :art:

--- spiderProject/CH1_spider_Introduction/requests_lib/request_lib.py
+++ spiderProject/CH1_spider_Introduction/requests_lib/request_lib.py
@@ -4,7 +4,7 @@
 import requests
 query = input('Input name:')
 
-url = f"https://www.sogou.com/web?query={query}"
+url = f"http://www.baidu.com/s?wd={query}"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
